7c41080813c0873d4d53abe7a83f4f7e4cb1b877
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-09-06 19:02:56 +0800

    feat: add github oauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,46 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter()
+  const { data: session } = authClient.useSession();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/")
+          toast.success("Signed out successfully.")
+        },
+      }
+    })
+  }
+
+  async function signIn() {
+    router.push("/login")
+  }
+
   return (
     <div>
       Hello World
       <ThemeToggle />
+
+      { session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>
+            Logout
+          </Button>
+        </div>
+      ) : (
+          <Button onClick={signIn}>Login</Button>
+        )
+      }
     </div>
   );
 }
