59714b4b7743b0294131838bd98c8b6fc8354962
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-09-08 18:54:33 +0800

    fix: missing avatar when using email as auth method

--- app/(home)/_components/Navbar.tsx
+++ app/(home)/_components/Navbar.tsx
@@ -42,7 +42,14 @@ export function Navbar() {
                         <ThemeToggle />
 
                         {isPending ? null : session ? (
-                           <UserDropdown email={session.user.email} image={session.user.image || ""} name={session.user.name}  />
+                           <UserDropdown 
+                                email={session.user.email} 
+                                image={session?.user.image ?? `https://avatar.vercel.sh/${session?.user.email}`} 
+                                name={
+                                    session?.user.name && session.user.name.length > 0 
+                                    ? session.user.name 
+                                    : session?.user.email.split("@")[0]
+                                }  />
                         ) : (
                             <>
                             <Link 
