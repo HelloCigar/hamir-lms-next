4cb9262980796a8555b92e3c74312fddc0b024a4
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-09-09 14:06:03 +0800

    feat: use better-auth admin plugin to protect admin routes

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,5 @@
-import 'server-only'
-
 import { betterAuth } from "better-auth";
-import { emailOTP } from "better-auth/plugins"
+import { emailOTP, admin } from "better-auth/plugins"
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./db";
 import { env } from "./env";
@@ -29,5 +27,6 @@ export const auth = betterAuth({
                 })
            }, 
         }),
+        admin()
     ]
 });
