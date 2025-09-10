5d3dbebef0e3dd2c849333e20a7d13b03a3a8dd5
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-09-10 10:27:45 +0800

    chore: secure edit course server action

--- app/admin/courses/[courseId]/edit/actions.ts
+++ app/admin/courses/[courseId]/edit/actions.ts
@@ -1,15 +1,48 @@
 "use server"
 
 import { requireAdmin } from "@/app/data/admin/require-admin"
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
 import { prisma } from "@/lib/db";
 import { ApiResponse } from "@/lib/types";
 import { courseSchema, CourseSchemaType } from "@/lib/zodSchemas";
+import { request } from "@arcjet/next";
 
+const aj = arcjet
+.withRule(
+    detectBot({
+        mode: 'LIVE',
+        allow: [],
+    })
+).withRule(
+    fixedWindow({
+        mode: 'LIVE',
+        window: "1m",
+        max: 5,
+    })
+)
 export async function editCourse(data: CourseSchemaType, courseId: string): Promise<ApiResponse> {
     const session = await requireAdmin();
 
     try {
+        const req =  await request();
+        const decision = await aj.protect(req, {
+            fingerprint: session.user.id
+        })
 
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()) {
+                return {
+                    status: 'error',
+                    message: "You have been blocked due to rate limiting",
+                }
+            } else {
+                return {
+                    status: 'error',
+                    message: 'You are probably a bot. Contact our suppport if your think this is a mistake'
+                }
+            }
+        }
+        
         const result = courseSchema.safeParse(data);
 
         if (!result.success) {
