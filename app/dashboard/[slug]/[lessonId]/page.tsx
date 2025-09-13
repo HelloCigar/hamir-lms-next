5988a7284adcd69d643794d564caabd839981d06
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-09-13 20:09:21 +0800

    feat: added sidebar for course and lesson viewing

---
+++ app/dashboard/[slug]/[lessonId]/page.tsx
@@ -0,0 +1,19 @@
+import { getLessonContent } from "@/app/data/course/get-lesson-content"
+
+type Params = Promise<{ lessonId: string }>
+
+export default async function LessonContentPage({
+    params
+}: {
+    params: Params
+}) {
+    const { lessonId } = await params;
+    const data = await getLessonContent(lessonId);
+
+
+    return (
+        <div>
+            <h1>{data.title}</h1>
+        </div>
+    )
+}
