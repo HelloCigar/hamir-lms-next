a447bc3c6eee77789d8516add843c8cf468e6ef8
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-09-14 20:41:45 +0800

    fix: change to tigris env

--- lib/S3Client.ts
+++ lib/S3Client.ts
@@ -4,7 +4,11 @@ import { S3Client } from '@aws-sdk/client-s3'
 import { env } from './env'
 
 export const S3 = new S3Client({
-    region: env.AWS_REGION,
-    endpoint: env.AWS_ENDPOINT_URL_S3,
-    forcePathStyle: false
+    region: env.TIGRIS_REGION,
+    endpoint: env.TIGRIS_ENDPOINT_URL_S3,
+    forcePathStyle: false,
+    credentials: {
+        accessKeyId: env.TIGRIS_ACCESS_KEY_ID,
+        secretAccessKey: env.TIGRIS_SECRET_ACCESS_KEY
+    }
 })
