b6cf46dd81b7b0f0b60aa0273cee52300e2bd39c
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-09-16 13:03:07 +0800

    style: show progress bar on page transition

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import Providers from './providers';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-         <ThemeProvider
+         <Providers>
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
@@ -38,6 +40,7 @@ export default function RootLayout({
             {children}
             <Toaster />
           </ThemeProvider>
+         </Providers>
       </body>
     </html>
   );
